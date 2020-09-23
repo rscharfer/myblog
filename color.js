e2694f2eb0e4aac59207802742955a2976deaf01
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2020-09-23 14:23:45 +0200

    added styling to the header

---
+++ color.js
@@ -0,0 +1,7 @@
+export const TOMATO = '#FF6347';
+export const BLUE_TOMATO = '#47E3FF'
+
+export const DARK_GRAY = '#222222'
+
+
+
--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,25 @@
 import Link from "next/link";
+import { TOMATO, BLUE_TOMATO } from "../color";
 
 export default function Header() {
   return (
     <>
+      <style jsx>
+        {`
+          .nav {
+            background: ${TOMATO};
+            height: 80px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 0 2rem;
+          }
+          a {
+            text-decoration: none;
+            color: ${BLUE_TOMATO};
+          }
+        `}
+      </style>
       <header className="header">
         <nav className="nav">
           <Link href="/">
