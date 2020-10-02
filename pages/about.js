9aed9ac8c0da7059a4c1dcd5826dfb486f563c09
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2020-10-02 17:37:10 +0200

    fixed problem

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 
-const About = ({ title, description }) => (
-  <Layout docTitle={`${title} | About`}>
+const About = ({ docTitle, description }) => (
+  <Layout docTitle={`${docTitle} | About`}>
     <p className="description">{description} </p>
     <p>I live with my wife and two kids in Hamburg. 👨‍👨‍👧‍👦 </p>
   </Layout>
@@ -11,7 +11,7 @@ export async function getStaticProps() {
   const configData = await import("../siteconfig.json");
   return {
     props: {
-      title: configData.default.title,
+      docTitle: configData.default.docTitle,
       description: configData.default.description,
     },
   };
